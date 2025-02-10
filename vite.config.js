3ac4042350f6e06f70980846af58cb88a0247031
yuki5718 <hayate8575@gmail.com>
2025-02-11 01:33:13 +0800

    修改repo路徑

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/{gh-repo name}/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/{React-week4}/' : '/',
   plugins: [react()],
 })
