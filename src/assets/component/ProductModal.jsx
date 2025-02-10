8f7a0e3eb5da7479ac33be6cdf2d969e6dcfbc6d
yuki5718 <hayate8575@gmail.com>
2025-02-11 02:00:24 +0800

    刪除inport未使用的套件

--- src/assets/component/ProductModal.jsx
+++ src/assets/component/ProductModal.jsx
@@ -1,7 +1,6 @@
 import { useState , useRef , useEffect} from 'react'
 import { Modal } from 'bootstrap'
 import axios from 'axios'
-import Swal from 'sweetalert2'
 
 const { VITE_BASE_URL , VITE_API_PAHT } = import.meta.env;
 
